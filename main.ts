d6bca140b60c7c729232e1d78fc01904beba3203
Leonardo Vilela Pinheiro <leonardo@vilelapinheiro.com>
2024-09-28 15:28:31 -0300

    wip

--- main.ts
+++ main.ts
@@ -1,4 +1,4 @@
-import { App, Editor, MarkdownView, Modal, Notice, Plugin, PluginSettingTab, Setting } from 'obsidian';
+import {MarkdownView, Menu, Notice, Plugin, TAbstractFile, WorkspaceLeaf} from 'obsidian';
 
 // Remember to rename these classes and interfaces!
 
@@ -14,121 +14,130 @@ export default class MyPlugin extends Plugin {
 	settings: MyPluginSettings;
 
 	async onload() {
-		await this.loadSettings();
 
-		// This creates an icon in the left ribbon.
-		const ribbonIconEl = this.addRibbonIcon('dice', 'Sample Plugin', (evt: MouseEvent) => {
-			// Called when the user clicks the icon.
-			new Notice('This is a notice!');
-		});
-		// Perform additional things with the ribbon
-		ribbonIconEl.addClass('my-plugin-ribbon-class');
 
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
-		const statusBarItemEl = this.addStatusBarItem();
-		statusBarItemEl.setText('Status Bar Text');
+		// this.addRibbonIcon("dice", "Print to console", () => {
+		// 	console.log("Hello, you! 2");
+		// 	new Notice('Olá');
+		// });
 
-		// This adds a simple command that can be triggered anywhere
-		this.addCommand({
-			id: 'open-sample-modal-simple',
-			name: 'Open sample modal (simple)',
-			callback: () => {
-				new SampleModal(this.app).open();
-			}
-		});
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			}
-		});
-		// This adds a complex command that can check whether the current state of the app allows execution of the command
-		this.addCommand({
-			id: 'open-sample-modal-complex',
-			name: 'Open sample modal (complex)',
-			checkCallback: (checking: boolean) => {
-				// Conditions to check
-				const markdownView = this.app.workspace.getActiveViewOfType(MarkdownView);
-				if (markdownView) {
-					// If checking is true, we're simply "checking" if the command can be run.
-					// If checking is false, then we want to actually perform the operation.
-					if (!checking) {
-						new SampleModal(this.app).open();
-					}
+		// this.registerEvent(
+		// 	this.app.workspace.on("file-menu", (menu: Menu, file: TAbstractFile, source: string, leaf?: WorkspaceLeaf) => {
+		// 		new Notice('Olá');
+		// 	})
+		// )
 
-					// This command will only show up in Command Palette when the check function returns true
-					return true;
+		// this.registerEvent(
+		// 	this.app.workspace.on("editor-menu", (menu, editor, view) => {
+		// 		new Notice('Olá');
+		// 		console.log("editor-menu triggered");
+		//
+		// 		if (!view || !(view instanceof MarkdownView)) {
+		// 			console.log("Not a Markdown view, skipping...");
+		// 			return;
+		// 		}
+		//
+		// 		menu.addItem((item) => {
+		// 			item
+		// 				.setTitle("LEONARDO")
+		// 				.setIcon("document")
+		// 				.onClick(async () => {
+		// 					console.log('olá mundo');
+		// 					new Notice(view?.file?.path ?? 'nada');
+						// });
+				// });
+			// })
+		// );
+		// new Notice('onload');
+
+		this.registerEvent(
+			this.app.workspace.on("editor-menu", (menu, editor, view) => {
+
+				// console.info('Clicou no menu de contexto');
+				if (!view) {
+					console.error('Não há view');
+					return;
 				}
+				if (!editor) {
+					console.error('Não há editor');
+					return;
+				}
+				const selectedText = editor.getSelection();
+				// console.log('Selecionou o texto:', selectedText);
+				const imageRegex = /!\[.*?\]\((https?:\/\/.*?)\)|<img .*?src="(https?:\/\/.*?)"/;
+				if (imageRegex.test(selectedText)) {
+					menu.addItem((item) => {
+						item.setTitle("Convert Image to Base64")
+							.setIcon("image-file")
+							.onClick(async () => {
+								// console.log('onClick');
+								return this.convertImageToBase64(editor, selectedText);
+							});
+					});
+				}
+			})
+		);
+	}
+
+	// onunload() {
+	//
+	// }
+
+	// async loadSettings() {
+	// 	this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
+	// }
+
+	// async saveSettings() {
+	// 	await this.saveData(this.settings);
+	// }
+
+	// Função para converter uma imagem selecionada em Base64
+	async convertImageToBase64(editor: CodeMirror.Editor, content: string) {
+		const imageRegex = /!\[.*?\]\((https?:\/\/.*?)\)|<img .*?src="(https?:\/\/.*?)"/g;
+		let match;
+		let newContent = content;
+
+		// Encontra a URL da imagem
+		while ((match = imageRegex.exec(content)) !== null) {
+			const url = match[1] || match[2];
+			// console.log('Vou tentar baixar:', url);
+
+			try {
+				// Baixar a imagem
+				const response = await fetch(url,
+					{mode: 'no-cors'}
+				);
+				console.log('response:', response);
+				// if (!response.ok) {
+				// 	new Notice('Error downloading image');
+				// 	return;
+				// }
+				const blob = await response.blob();
+				console.log('Consegui baixar o blob', blob);
+
+				// Converter para Base64
+				const base64 = await this.blobToBase64(blob);
+				const mimeType = blob.type;
+
+				// Substituir a URL pela string Base64
+				const base64String = `data:${mimeType};base64,${base64}`;
+				newContent = newContent.replace(url, base64String);
+
+				// Atualiza o conteúdo no editor
+				// editor.replaceSelection(newContent);
+			} catch (error) {
+				console.error(`Erro ao converter imagem: ${url}`, error);
 			}
+		}
+	}
+
+	// Função auxiliar para converter Blob em Base64
+	async blobToBase64(blob: Blob): Promise<string> {
+		return new Promise((resolve, reject) => {
+			const reader = new FileReader();
+			reader.onloadend = () => resolve(reader.result!.toString().split(',')[1]);
+			reader.onerror = reject;
+			reader.readAsDataURL(blob);
 		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
-		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000));
-	}
-
-	onunload() {
-
-	}
-
-	async loadSettings() {
-		this.settings = Object.assign({}, DEFAULT_SETTINGS, await this.loadData());
-	}
-
-	async saveSettings() {
-		await this.saveData(this.settings);
-	}
-}
-
-class SampleModal extends Modal {
-	constructor(app: App) {
-		super(app);
-	}
-
-	onOpen() {
-		const {contentEl} = this;
-		contentEl.setText('Woah!');
-	}
-
-	onClose() {
-		const {contentEl} = this;
-		contentEl.empty();
-	}
-}
-
-class SampleSettingTab extends PluginSettingTab {
-	plugin: MyPlugin;
-
-	constructor(app: App, plugin: MyPlugin) {
-		super(app, plugin);
-		this.plugin = plugin;
-	}
-
-	display(): void {
-		const {containerEl} = this;
-
-		containerEl.empty();
-
-		new Setting(containerEl)
-			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
 	}
 }
